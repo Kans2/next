282c95359b3f0fe8b3be1abc98054731bf8d67b5
kannan <kannanskannans400@gmail.com>
2024-02-24 17:11:48 +0530

    updated

---
+++ advanced/script.js
@@ -0,0 +1,93 @@
+//Template litereals  strings
+/*
+function greetings() {
+  return `hello buddy`;
+}
+
+console.log(`greetings : ${greetings()}`); 
+//practice
+
+const Template = `the quick 
+                  brown fox
+                  jumbs over
+                  the lazy dog
+                  `;
+
+let firstName = "super";
+let lastName = "man";
+console.log(`${firstName} ${lastName}`); 
+
+//arrow functions  es6
+
+greet = (username) => {
+  return `${username}`;
+};
+console.log(greet("i am on"));
+
+//practice
+
+setTimeout(() => {
+  console.log("hello");
+}, 2000); 
+
+//Enhance Object literials
+
+const user = (name, age, work) => {
+  //key and value has same
+  return {
+    name,
+    age,
+    work,
+    intro() {
+      console.log("working");
+    },
+  };
+};
+
+const person = user();
+
+console.log(person.intro()); 
+
+let lib = {
+  sum(a, b) {
+    return a + b;
+  },
+  multi(a, b) {
+    return a * b;
+  },
+};
+
+console.log(lib.sum(4, 7));
+console.log(lib.multi(2, 2)); 
+
+const getperson = (name, age, height) => {
+  return {
+    name,
+    age,
+    height,
+  };
+};
+
+const person = getperson("dev", 19, 128);
+console.log(person);
+
+
+//Default Parameters
+
+const count = (count = false) => {
+  if (count == true) {
+    for (let i = 0; i < 5; i++) {
+      console.log(`Count is ${i}`);
+    }
+  }
+};
+count(true); 
+
+//practice
+
+const multiply = (a, b = 8) => {
+  return a * b;
+};
+
+const res = multiply(2);
+console.log(res); */
