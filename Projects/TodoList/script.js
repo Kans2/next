8cf8971081036aaa5ce1856c1fd40f5ea95e52d2
kannan <kannanskannans400@gmail.com>
2024-04-29 21:40:18 +0530

    updated

--- Projects/TodoList/script.js
+++ Projects/TodoList/script.js
@@ -1,13 +1,39 @@
-const input = document.querySelector("input");
-const Additem = document.querySelector("button");
+let input = document.querySelector("input");
+let Additem = document.querySelector("button");
 let task = document.querySelector(".todos");
+let form = document.querySelector("form");
 
-function getTodo() {}
+function getTodo(value) {
+  //Creating New Elements
+  let todo = document.createElement("div");
+  let textEI = document.createElement("span");
 
-Additem.addEventListener("click", (e) => {
+  //Setting values and styles
+
+  textEI.innerHTML = value;
+
+  //Append our Element to the DOM
+
+  todo.appendChild(textEI);
+
+  let closeEI = document.createElement("span");
+  closeEI.innerHTML = "&times;";
+  closeEI.classList.add("delete");
+
+  //Attach Events
+  closeEI.addEventListener("click", function (e) {
+    task.removeChild(todo);
+  });
+
+  todo.appendChild(closeEI);
+  todo.classList.add("todo");
+  return todo;
+}
+
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  task.innerHTML = `
-  ${input.value}
-    `;
+  let value = input.value;
+  if (!value.trim()) return;
+  task.appendChild(getTodo(value));
+  input.value = "";
 });
