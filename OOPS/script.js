fcf22039f29ea240d4159e03d7398f82e8042aad
kannan <kannanskannans400@gmail.com>
2024-05-04 15:37:49 +0530

    updated

--- OOPS/script.js
+++ OOPS/script.js
@@ -48,4 +48,63 @@ const res = person.greetRegular();
 const out = person.greetArrow();
 
 console.log(res);
-console.log(out);   */
+console.log(out);*/
+
+//Factory function is type function that is used to create and return the objects.
+//Its design pattern that provides an Alternative way to create objects compared to using constructors and the new keyword..
+/*
+function createProfile(firstName, Age, experience) {
+  return {
+    Name: firstName,
+    Age: Age,
+    year: experience,
+    function: function () {
+      console.log(
+        `Hi buddy! My name is ${this.Name}.  I am just ${this.Age} year old.I am in this field in ${this.year}.`
+      );
+    },
+  };
+}
+
+const person = createProfile("jhon", 30, 2);
+
+//console.log(person);
+//person.function();
+
+//practice
+function vechicles(type, brand, model, year) {
+  return {
+    type1: type,
+    brand1: brand,
+    model1: model,
+    year1: year,
+  };
+}
+const vehicle1 = vechicles("racing cars", "Koesnieg", "Supra", 2024);
+
+console.log(vehicle1);  */
+
+//CONSTRUCTOR
+//-- Constructor is regular function use with    $new keyword   to create and initialize objects shared properties and method.
+//--Blue brint for creating multiple instances of object structre and behaviour.
+
+function PlayerHistory(score, matches, wins) {
+  this.score = score;
+  this.matches = matches;
+  this.wins = wins;
+  this.info = function () {
+    console.log(
+      `the player score is ${this.score}. going to the Action in ${this.matches}.to Winning the matches are ${this.wins}`
+    );
+  };
+}
+
+//NEW KEYWORD
+//1. First create empty object
+//2. sets "this" to point to that object
+//3.We can omit return statement using "new" keyword
+
+const player1 = new PlayerHistory(1000, 100, 100);
+
+//console.log(player1);
+player1.info();
