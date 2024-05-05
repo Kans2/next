6dde63ca7734e94f955a9c7c70e7663ce8f75815
kannan <kannanskannans400@gmail.com>
2024-05-05 11:03:55 +0530

    updated

--- OOPS/script.js
+++ OOPS/script.js
@@ -88,6 +88,7 @@ console.log(vehicle1);  */
 //-- Constructor is regular function use with    $new keyword   to create and initialize objects shared properties and method.
 //--Blue brint for creating multiple instances of object structre and behaviour.
 
+/*
 function PlayerHistory(score, matches, wins) {
   this.score = score;
   this.matches = matches;
@@ -107,4 +108,125 @@ function PlayerHistory(score, matches, wins) {
 const player1 = new PlayerHistory(1000, 100, 100);
 
 //console.log(player1);
-player1.info();
+player1.info();   
+
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.getInfo = function () {
+    return `${this.title} by ${this.author} ${this.year}`;
+  };
+}
+
+const Book1 = new Book("Love", "miles", 2005);
+
+const res = Book1.getInfo();
+
+console.log(Book1); 
+
+//PRACTICE
+function Person(name, age, gender) {
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+  this.info = function () {
+    return `The Name is ${this.name}. age is ${this.age} gender Is ${this.gender}`;
+  };
+}
+
+const person1 = new Person("robert", 20, "Male");
+
+console.log(person1);
+
+console.log(person1.info());  
+
+function Car(make, model, year, color) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.Strat = function () {
+    return `Strarting the ${make} ${model}...`;
+  };
+  this.Stop = function () {
+    return `Stooping the ${make} ${model}.`;
+  };
+}
+
+const car1 = new Car("Supra", "Gtr-7");
+
+console.log(car1.Strat()); 
+console.log(car1.Stop()); */
+
+//BUILT IN CONSTRUCTORS
+// native-constructors,Create object of various data type. available in Globaly do not need explicity
+//Primitive data types & built in objects.
+/*
+//Data types
+const num1 = new Number(100);
+console.log(num1);
+const st1 = new String("developer");
+console.log(st1);
+const bool = new Boolean(false);
+console.log(bool);
+const Arr = new Array(1, 2, 3, "hi");
+console.log(typeof Arr); */
+
+/*
+//OBJECT.CREATE() --> method creates a new object, using an prototype of newly created object
+
+let person = {
+  greet: function () {
+    return `Hi! my name is ${this.Name} ${this.lastName}`;
+  },
+};
+
+const person1 = Object.create(person);
+
+person1.Name = "kannan";
+person1.lastName = "developer";
+
+const res = person1.greet();
+console.log(res);
+
+let jhon = Object.create(person, {
+  Name: { value: "Jhon" },
+  lastName: { value: "Doe" },
+});
+
+const res1 = jhon.greet();
+console.log(jhon);
+console.log(res1);  */
+
+//PROTOTYPE
+
+//every javascript object has anonyms proprty called prototype
+//Remember functions & arrays are also a object
+//this is an Parent of an object, protype is object itself, Special properties and Methods
+
+//const obj = {};
+//console.log(obj);
+
+//We can check prototype
+//--> obj._proto_
+//-->obj.constructor.protoype()
+//-->Object.getPrototypeOf(a)
+
+//console.log(obj.__proto__);
+
+//********************* Changing the built in Method
+/*
+Array.prototype.pop = function () {
+  return "Pop it up baby";
+};
+
+const myArr = ["one", "two", "three"];
+console.log(myArr.pop());  
+
+String.prototype.hello = function () {
+  console.log(this);
+  console.log(this.toUpperCase());
+};
+
+console.log("hello and welcome".hello());  */
